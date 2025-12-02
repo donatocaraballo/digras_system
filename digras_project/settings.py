f710e0d960bc6ae5819449a5d15f7638045fe0e8
Donato Caraballo <109986794+donatocaraballo@users.noreply.github.com>
2025-12-02 17:41:13 -0400

    Configure inventory and purchases integration

--- digras_project/settings.py
+++ digras_project/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'base',
-    'inventario',
-    'compras',
+    'inventario.apps.InventarioConfig',
+    'compras.apps.ComprasConfig',
     'corsheaders',
 ]
 
